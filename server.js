3d39171d69257d7ab71026ad03d8cdbd497e5d30
smox14 <kantarat.htkkw@gmail.com>
2022-03-16 10:06:28 +1100

    build server

--- server.js
+++ server.js
@@ -9,3 +9,23 @@ const port = 300
 app.listen(port, 
   () => console.log(`listening on port ${port}`)
 )
+
+// middlewares
+const logger = require('./middlewares/logger') 
+
+// controllers
+
+
+
+
+// (middleware)  log request 
+app.use(logger)
+
+// (middleware) (Single-Page Application)
+app.use(express.static('client'))
+
+// parse json body in a POST, PUT or DELETE request, and it assigns the data to req.body
+app.use(express.json())
+
+
+// API routes - controller files
