02d072371ecf8a2cd2892471a4448ee4e1c2daed
cchau <chun.wing.chau@gmail.com>
2022-03-17 10:37:40 +1100

    in progress

--- models/question.js
+++ models/question.js
@@ -1,15 +1,20 @@
-
-// function checkAnswer() {
-//   if document.querySelector
-// }
 const db = require('../db/db')
-  
-function createAnswer(answer, question, category, user_answer, question_id, category_id, value, user_id) {
-  const sql = "INSERT INTO user_questions(answer, question, category, user_answer, question_id, category_id, value, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)"
 
-  return db 
-    .query(sql, [answer, question, category, user_answer, question_id, category_id, value, user_id])
-    .then(dbRes => dbRes.rows[0])
+Question = {
+  createAnswer: (answer, question, category, user_answer, question_id, category_id, value, user_id) => {
+    const sql = "INSERT INTO user_questions(answer, question, category, user_answer, question_id, category_id, value, user_id) VALUES ($1, $2, $3, $4, $5, $6, $7, $8)"
+
+    return db
+      .query(sql, [answer, question, category, user_answer, question_id, category_id, value, user_id])
+      .then(dbRes => dbRes.rows[0])
+  },
+  selectAll: () => {
+    const sql = "SELECT * FROM user_answers"
+    return db
+      .query(sql)
+      .then(dbRes => dbRes.rows[0])
+  }
 }
 
-module.exports = createAnswer
+
+module.exports = Question
