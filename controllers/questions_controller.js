ffa22b14dca9a2fd712ca6cb0cbb0faed727f96a
razorsharp021 <reesarmani@gmail.com>
2022-03-16 14:28:29 +1100

    database update

--- controllers/questions_controller.js
+++ controllers/questions_controller.js
@@ -1,12 +1,3 @@
 const express = require('express')
-const Treasure = require('../models/question')
+const createAnswer = require('../models/question')
 
-const router = express.Router()
-
-router.get('/', (req, res) => {
-  Quiz
-    .findQuestion()
-    .then(question => res.json(question))
-})
-
-module.exports = router
