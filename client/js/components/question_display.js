b70fd4934545947639333376b1d0e10bfa6d94c6
smox14 <kantarat.htkkw@gmail.com>
2022-03-17 12:30:47 +1100

    edit: display

--- client/js/components/question_display.js
+++ client/js/components/question_display.js
@@ -2,11 +2,14 @@ function renderQuizBoard(){
   document.querySelector('#page').innerHTML = 
     `
     <section class="question-board">
-    <div onClick=getQuestion(100)>100</div>
-    <div onClick=getQuestion(200)>200</div>
-    <div onClick=getQuestion(300)>300</div>
-    <div onClick=getQuestion(400)>400</div>
-    <div onClick=getQuestion(500)>500</div>
+    <div onClick=getQuestion(100)>$100</div>
+    <div onClick=getQuestion(200)>$200</div>
+    <div onClick=getQuestion(300)>$300</div>
+    <div onClick=getQuestion(400)>$400</div>
+    <div onClick=getQuestion(500)>$500</div>
+    <div onClick=getQuestion(600)>$600</div>
+    <div onClick=getQuestion(800)>$800</div>
+    <div onClick=getQuestion(1000)>$1000</div>
     </section>
     <section class="create-question"></section>
   `
@@ -24,14 +27,14 @@ function renderQuizQuestion() {
             <p>Unique ID: ${state.question.id}</p>
             <p id="question">Question: ${state.question.question}</p>
             <input type="hidden" name="question" value="${state.question.question.replaceAll('\"', "\'")}">
-            <p>Answer: ${state.question.answer}</p>
+            <p>Answer: ${state.question.answer}</p> 
             <input type="hidden" name="question_answer" class="question_answer" value="${state.question.answer}">
             <input name="user_input" class="user_input"></input><br>
             <button class="answer-btn">Submit</button>
             
       </form>
-        <button style="visibility:hidden;" class="next-btn" onClick="getQuestion()">Next</button>
-      
+        <button style="visibility:hidden;" class="next-btn" onClick="getQuestion(${state.question.value})">Next</button>
+      <section class="user-answer"> </section>
     
     `
 }
@@ -45,16 +48,6 @@ function grabQuestion(event) {
   document.querySelector('.answer-btn').style.visibility = "hidden"
   document.querySelector('.next-btn').style.visibility = "visible"
 
-  //  check user input
-  if (state.question.answer === data.user_input) {
-    state.question.user_answer = true
-  } else {
-    state.question.user_answer = false
-  }
-    let obj = state.question 
-    obj.user_id = state.userId
-    axios
-        .post('/api/questions', obj)
-        .then(res => res.data)
-
+  checkUserAnswer(data.user_input)  
 }
+
---
+++ client/js/components/user_answer.js
@@ -0,0 +1,32 @@
+function checkUserAnswer(user_input) {
+  if (state.question.answer.toLowerCase() === user_input.toLowerCase()) {
+    state.question.user_answer = true
+  } else {
+    state.question.user_answer = false
+  }
+  saveToDatabase()
+  renderUserAnswerResult(state.question.user_answer)
+} 
+
+function saveToDatabase(){
+  let obj = state.question 
+    obj.user_id = state.userId
+    axios
+        .post('/api/questions', obj)
+        .then(res => res.data)
+}
+
+function renderUserAnswerResult(userAnswer){
+  let userAnswerDOM = document.querySelector('.user-answer')
+  userAnswerDOM.style.visibility = "visible"
+  if(userAnswer){
+    state.userScore += parseInt(state.question.value)
+    userAnswerDOM.innerHTML = `<h2> CORRECT! </h2>`
+  } else {
+    userAnswerDOM.innerHTML = `
+      <h2>INCORRECT!</h2>
+      <p> The Answer: ${state.question.answer}</p>
+    `
+  }
+  
+}
